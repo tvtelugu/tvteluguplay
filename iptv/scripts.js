9b875c1604d43fc27c4c8d55321fd007d00d87f5
TVTelugu <63017421+tvtelugu@users.noreply.github.com>
2024-08-07 23:32:39 +0530

    Update scripts.js

--- iptv/scripts.js
+++ iptv/scripts.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetch('iptv.json')
-        .then(response => response.json())
-        .then(data => loadChannels(data));
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => loadChannels(data))
+        .catch(error => console.error('Error fetching data:', error));
 });
 
 function loadChannels(channels) {
     const channelGrid = document.getElementById('channelGrid');
+    channelGrid.innerHTML = ''; // Clear existing content
     channels.forEach(channel => {
         const channelCard = document.createElement('div');
         channelCard.className = 'channel-card';
